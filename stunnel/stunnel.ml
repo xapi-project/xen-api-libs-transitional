a3617b581ce2bb8e5eac284d83b131b67230db40
Kun Ma <kun.ma@citrix.com>
2018-08-07 16:17:01 +0800

    CP-29015: Set correct ciphers for stunnel client
    Signed-off-by: Kun Ma <kun.ma@citrix.com>

--- stunnel/stunnel.ml
+++ stunnel/stunnel.ml
@@ -43,7 +43,7 @@ let legacy_protocol_and_ciphersuites_allowed = ref false
 let is_legacy_protocol_and_ciphersuites_allowed () =
   !legacy_protocol_and_ciphersuites_allowed
 
-let good_ciphersuites = ref (Some "!EXPORT:RSA+AES128-SHA256")
+let good_ciphersuites = ref (Some "!EXPORT:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-SHA384:RSA+AES128-SHA256:AES128-SHA:AES256-SHA256")
 let legacy_ciphersuites = ref "RSA+AES256-SHA:RSA+AES128-SHA:RSA+RC4-SHA:RSA+DES-CBC3-SHA"
 
 let init_stunnel_path () =
@@ -161,10 +161,12 @@ let config_file verify_cert extended_diagnosis host port legacy =
         "sslVersion = all";
         "options = NO_SSLv2";
         "options = NO_SSLv3";
-        "ciphers = " ^ (good_ciphers ()) ^ (match !legacy_ciphersuites with "" -> "" | s -> (":" ^ s))
+        "ciphers = " ^ (good_ciphers ()) ^ (match !legacy_ciphersuites with "" -> "" | s -> (":" ^ s));
+        "curve = secp384r1"
       ] else [
         "sslVersion = TLSv1.2";
         "ciphers = " ^ (good_ciphers ());
+        "curve = secp384r1"
       ]
     )
   in
