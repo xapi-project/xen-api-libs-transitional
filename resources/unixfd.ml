d55a26843eda47eea1892678fa5445a4a14cb856
Rob Hoes <rob.hoes@citrix.com>
2022-08-17 14:38:31 +0000

    Reformat
    Signed-off-by: Rob Hoes <rob.hoes@citrix.com>

--- resources/unixfd.ml
+++ resources/unixfd.ml
@@ -29,7 +29,8 @@ let on_finalise_leaked (chan, loc) =
   with e ->
     if enabled then (
       Printexc.print_backtrace stderr ;
-      Printf.eprintf "[unix_fd]: close failed: %s (allocated at %s)\n%!" (Printexc.to_string e) loc
+      Printf.eprintf "[unix_fd]: close failed: %s (allocated at %s)\n%!"
+        (Printexc.to_string e) loc
     )
 
 let within chan ~loc =
@@ -49,21 +50,21 @@ let with_fd alloc = alloc () |> within
 let with_open_connection addr ~loc f =
   let open Unix in
   with_fd ~loc (fun () ->
-      socket ~cloexec:true (domain_of_sockaddr addr) SOCK_STREAM 0)
+      socket ~cloexec:true (domain_of_sockaddr addr) SOCK_STREAM 0
+  )
   @@ fun s -> connect !s addr ; f s
 
 let with_ic fd =
   (* A file descriptor cannot be safely shared between an [in] and [out] channel.
    * Unix.open_connection does this but if you close both channels you get EBADF.
-  *)
+   *)
   Safe.within
-  @@ Safe.create ~release:close_in_noerr
-    (Unix.in_channel_of_descr (Unix.dup fd))
+  @@ Safe.create ~release:close_in_noerr (Unix.in_channel_of_descr (Unix.dup fd))
 
 let with_oc fd =
   Safe.within
   @@ Safe.create ~release:close_out_noerr
-    (Unix.out_channel_of_descr (Unix.dup fd))
+       (Unix.out_channel_of_descr (Unix.dup fd))
 
 let with_channels t f =
   let fd = !t in
--- xenctrlext/xenctrlext.mli
+++ xenctrlext/xenctrlext.mli
@@ -14,33 +14,44 @@
 
 open Xenctrl
 
-external get_boot_cpufeatures: handle ->  (int32 * int32 * int32 * int32 * int32 * int32 * int32 * int32) = "stub_xenctrlext_get_boot_cpufeatures" 
+external get_boot_cpufeatures :
+  handle -> int32 * int32 * int32 * int32 * int32 * int32 * int32 * int32
+  = "stub_xenctrlext_get_boot_cpufeatures"
 
-external domain_set_timer_mode: handle -> domid -> int -> unit = "stub_xenctrlext_domain_set_timer_mode"
+external domain_set_timer_mode : handle -> domid -> int -> unit
+  = "stub_xenctrlext_domain_set_timer_mode"
 
-external domain_send_s3resume: handle -> domid -> unit = "stub_xenctrlext_domain_send_s3resume"
-external domain_get_acpi_s_state: handle -> domid -> int = "stub_xenctrlext_domain_get_acpi_s_state"
+external domain_send_s3resume : handle -> domid -> unit
+  = "stub_xenctrlext_domain_send_s3resume"
 
-external domain_trigger_power: handle -> domid -> unit = "stub_xenctrlext_domain_trigger_power"
-external domain_trigger_sleep: handle -> domid -> unit = "stub_xenctrlext_domain_trigger_sleep"
+external domain_get_acpi_s_state : handle -> domid -> int
+  = "stub_xenctrlext_domain_get_acpi_s_state"
 
-external domain_suppress_spurious_page_faults: handle -> domid -> unit = "stub_xenctrlext_domain_suppress_spurious_page_faults"
+external domain_trigger_power : handle -> domid -> unit
+  = "stub_xenctrlext_domain_trigger_power"
+
+external domain_trigger_sleep : handle -> domid -> unit
+  = "stub_xenctrlext_domain_trigger_sleep"
+
+external domain_suppress_spurious_page_faults : handle -> domid -> unit
+  = "stub_xenctrlext_domain_suppress_spurious_page_faults"
 
 type runstateinfo = {
-  state : int32;
-  missed_changes: int32;
-  state_entry_time : int64;
-  time0 : int64;
-  time1 : int64;
-  time2 : int64;
-  time3 : int64;
-  time4 : int64;
-  time5 : int64;
+    state: int32
+  ; missed_changes: int32
+  ; state_entry_time: int64
+  ; time0: int64
+  ; time1: int64
+  ; time2: int64
+  ; time3: int64
+  ; time4: int64
+  ; time5: int64
 }
 
-external domain_get_runstate_info : handle -> int -> runstateinfo = "stub_xenctrlext_get_runstate_info"
+external domain_get_runstate_info : handle -> int -> runstateinfo
+  = "stub_xenctrlext_get_runstate_info"
 
-external get_max_nr_cpus: handle -> int = "stub_xenctrlext_get_max_nr_cpus"
+external get_max_nr_cpus : handle -> int = "stub_xenctrlext_get_max_nr_cpus"
 
-external domain_set_target: handle -> domid -> domid -> unit = "stub_xenctrlext_domain_set_target"
-
+external domain_set_target : handle -> domid -> domid -> unit
+  = "stub_xenctrlext_domain_set_target"
